60ae864375e1b77717b5dffba927f364e75b4f95
Nolan Bowen <nbowen1@terpmail.umd.edu>
2023-11-06 18:07:52 -0500

    created new file for check-in and added my calculate points method

---
+++ final.py
@@ -0,0 +1,19 @@
+class Game:
+    """
+    features of our game class that allows the user to play
+    the anagram game
+    """
+    def calculate_points(self,word):
+        """ Calculates the points the user gets based on length of word found
+
+        Args:
+            word (str): word found by user
+
+        Returns:
+            str: points based on length of word found
+        """
+        word_len = len(word)
+        if word_len >= self.word:
+            return(word_len - 2) * 100
+        else:
+            return 0
