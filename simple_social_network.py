a76966b9775558dd47a6b5b36a1fb1e9499a15cb
NolBow341 <101525499+NolBow341@users.noreply.github.com>
2023-12-11 14:23:35 -0500

    Delete simple_social_network.py

--- simple_social_network.py
+++
@@ -1,9 +0,0 @@
-class Person:
-     """example
-     """
-     
-     def __init__(self, name, age, gender, username):
-          self.name = name
-          self.age = age 
-          self.gender = gender
-          self.username = username
